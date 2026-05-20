7a47781e48de9e57c571c5ce9d97a2596ab3ceec
Ryan Zhiyang Tan <ryatan@c2r2s9.42singapore.sg>
2026-05-20 15:56:23 +0800

    remove logging

--- src/prompt.c
+++ src/prompt.c
@@ -10,14 +10,14 @@ int	prompt_loop(const char *prompt)
 	while (1)
 	{
 		rl_return = readline(prompt);
-		log_print("String read from prompt:", rl_return);
+		//log_print("String read from prompt:", rl_return);
 		input_split = ft_split(rl_return, ' ');
 		if (!input_split)
 		{
 			free_all(input_split);
 			return (1);
 		}
-		log_split(input_split);
+		//log_split(input_split);
 		if (!rl_return)
 		{
 			perror("Error-> readline() returns NULL");
